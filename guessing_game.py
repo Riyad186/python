ee51d9140d085227255028f7ea91bb4395a9c952
riyad <riyadislam16@gmail.com>
2022-10-07 23:24:22 +0600

    Guessing game

---
+++ guessing_game.py
@@ -0,0 +1,19 @@
+secret_number = 9
+guess_count = 0
+guess_limit =3
+count =0
+
+while guess_count<guess_limit:
+    guess = int(input("guess:"))
+    guess_count +=1
+    if guess == secret_number:
+        print("you won!")
+        count+=1
+    else:
+        print(f"lose {guess_count}th ")
+#print(f"you win only {count} time ")
+if count  !=0:
+    print(f"you won {count} time")
+else:
+    print('you lose')
+    
