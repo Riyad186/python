9fd9fc9219acaae9e71ff1cc124fbe0288bebcfb
riyad <riyadislam16@gmail.com>
2022-10-07 23:50:08 +0600

    mylogic

---
+++ car_game.py
@@ -0,0 +1,12 @@
+time = 0
+
+while time < 4:
+    car = str(input("car  doing:"))
+    if car == 'asd':
+        print("I don't understand that...")
+    elif car == 'start':
+        print("car started...Ready to go")
+    elif car =='stop':
+        print("car stopped")
+    elif car == 'quit':
+        break
